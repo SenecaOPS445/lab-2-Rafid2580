cf4516397bd4801ea50d9a7cba110b69f52b9251
Rafid Al Fahim <rfahim1@myseneca.ca>
2024-05-24 13:06:33 -0400

    lab2e.py

---
+++ lab2e.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# Author: Rafid Al Fahim
+# Author ID: Rfahim1
+# Date Created: 2024/05/24
+
+timer = 10
+while timer > 0:
+    print(timer)
+    timer = timer -1
+print ('blast off!')
